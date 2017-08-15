e4b6b3363859144df393e902255a15a3e23a8685
Eleanor Deal <eleanor.deal@gmail.com>
2017-08-15 16:24:23 +0100

    Add dataset and edition details for output instances

--- cmd/dp-recipe-api/main.go
+++ cmd/dp-recipe-api/main.go
@@ -42,11 +42,16 @@ type recipeListResponse struct {
 }
 
 type recipeResponse struct {
-	ID              string `json:"id"`
-	Alias           string `json:"alias"`
-	Format          string `json:"format"`
-	InputFiles      []file `json:"files"`
-	OutputInstances int    `json:"output_instances"`
+	ID              string     `json:"id"`
+	Alias           string     `json:"alias"`
+	Format          string     `json:"format"`
+	InputFiles      []file     `json:"files"`
+	OutputInstances []instance `json:"output_instances"`
+}
+
+type instance struct {
+	DatasetID string   `json:"dataset_id"`
+	Editions  []string `json:"editions"`
 }
 
 type file struct {
@@ -68,7 +73,12 @@ var cpiRecipe = recipeResponse{
 	InputFiles: []file{
 		{"CPI COICOP v4"},
 	},
-	OutputInstances: 1,
+	OutputInstances: []instance{
+		{
+			DatasetID: "931a8a2a-0dc8-42b6-a884-7b6054ed3b68",
+			Editions:  []string{"Time-series"},
+		},
+	},
 }
 
 func recipeListHandler(w http.ResponseWriter, req *http.Request) {
