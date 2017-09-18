33be53044e24d5257527ae7d82e8ca4b97bfb771
Matt Guest <mattguest1988@gmail.com>
2017-09-18 10:45:39 +0100

    Updated stubbed data to include code lists for CPI data.

--- cmd/dp-recipe-api/main.go
+++ cmd/dp-recipe-api/main.go
@@ -49,9 +49,16 @@ type recipeResponse struct {
 	OutputInstances []instance `json:"output_instances"`
 }
 
+type CodeList struct {
+	ID   string `json:"id"`
+	HRef string `json:"href"`
+	Name string `json:"name"`
+}
+
 type instance struct {
-	DatasetID string   `json:"dataset_id"`
-	Editions  []string `json:"editions"`
+	DatasetID string     `json:"dataset_id"`
+	Editions  []string   `json:"editions"`
+	CodeLists []CodeList `json:"code_lists"`
 }
 
 type file struct {
@@ -77,8 +84,12 @@ var cpiRecipe = recipeResponse{
 		{
 			DatasetID: "931a8a2a-0dc8-42b6-a884-7b6054ed3b68",
 			Editions:  []string{"Time-series"},
+			CodeLists: []CodeList{{ ID:"64d384f1-ea3b-445c-8fb8-aa453f96e58a", Name: "time", HRef: "http://localhost:22400/code-lists/64d384f1-ea3b-445c-8fb8-aa453f96e58a"},
+				{ID:"65107A9F-7DA3-4B41-A410-6F6D9FBD68C3", Name: "geography", HRef: "http://localhost:22400/code-lists/65107A9F-7DA3-4B41-A410-6F6D9FBD68C3"},
+				{ID:"e44de4c4-d39e-4e2f-942b-3ca10584d078", Name: "aggregate", HRef: "http://localhost:22400/code-lists/e44de4c4-d39e-4e2f-942b-3ca10584d078"}},
 		},
 	},
+
 }
 
 func recipeListHandler(w http.ResponseWriter, req *http.Request) {
