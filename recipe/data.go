8f8d94b5317f3413cc767f84832d74cacd4d6f5a
Geraint Edwards <gedgey@gmail.com>
2017-11-15 12:59:49 +0000

    add SAPE to recipes

--- recipe/data.go
+++ recipe/data.go
@@ -5,9 +5,9 @@ package recipe
 // - add the var name to the list of Items
 // - increment the Count and Total count in the RecipeList
 
-//FullLit of recipes available via this API
+//FullList of recipes available via this API
 var FullList = List{
-	Items:        []Response{CPI},
+	Items:        []Response{CPI, SAPE},
 	Count:        1,
 	TotalCount:   1,
 	ItemsPerPage: 10,
@@ -30,6 +30,25 @@ var CPI = Response{
 			CodeLists: []CodeList{{ID: "64d384f1-ea3b-445c-8fb8-aa453f96e58a", Name: "time", HRef: "http://localhost:22400/code-lists/64d384f1-ea3b-445c-8fb8-aa453f96e58a"},
 				{ID: "65107A9F-7DA3-4B41-A410-6F6D9FBD68C3", Name: "geography", HRef: "http://localhost:22400/code-lists/65107A9F-7DA3-4B41-A410-6F6D9FBD68C3"},
 				{ID: "e44de4c4-d39e-4e2f-942b-3ca10584d078", Name: "aggregate", HRef: "http://localhost:22400/code-lists/e44de4c4-d39e-4e2f-942b-3ca10584d078"}},
+		},
+	},
+}
+
+var SAPE = Response{
+	ID:     "8ef16b08-fef5-4bd3-8300-8066b0c777ce",
+	Alias:  "sape",
+	Format: "v4",
+	InputFiles: []file{
+		{"sape"},
+	},
+	OutputInstances: []instance{
+		{
+			DatasetID: "4e1a21a9-3fb9-4a71-b2ad-9be29457236b",
+			Editions:  []string{"Time-series"},
+			CodeLists: []CodeList{{ID: "64d384f1-ea3b-445c-8fb8-aa453f96e58a", Name: "time", HRef: "http://localhost:22400/code-lists/64d384f1-ea3b-445c-8fb8-aa453f96e58a"},
+				{ID: "65107A9f-7da3-4b41-a410-6f6d9fbd68c3", Name: "geography", HRef: "http://localhost:22400/code-lists/65107A9f-7da3-4b41-a410-6f6d9fbd68c3"},
+				{ID: "4e1a21a9-3fb9-4a71-b2ad-9be29457236b", Name: "age", HRef: "http://localhost:22400/code-lists/4e1a21a9-3fb9-4a71-b2ad-9be29457236b"},
+				{ID: "54ff5089-ea78-45ef-afa2-0dfe58f89497", Name: "sex", HRef: "http://localhost:22400/code-lists/54ff5089-ea78-45ef-afa2-0dfe58f89497"}},
 		},
 	},
 }
