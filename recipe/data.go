da143cf085a38664dcfe0374ebd08e34a4552455
Michael Adams <mikelovesbooks@gmail.com>
2017-12-05 11:33:25 +0000

    Update data.go
    change all to "time-series" as two were still "Time-series"

--- recipe/data.go
+++ recipe/data.go
@@ -25,7 +25,7 @@ var CPI = Response{
 	OutputInstances: []instance{
 		{
 			DatasetID: "931a8a2a-0dc8-42b6-a884-7b6054ed3b68",
-			Editions:  []string{"Time-series"},
+			Editions:  []string{"time-series"},
 			Title:     "UK consumer price inflation",
 			CodeLists: []CodeList{{ID: "64d384f1-ea3b-445c-8fb8-aa453f96e58a", Name: "time", HRef: "http://localhost:22400/code-lists/64d384f1-ea3b-445c-8fb8-aa453f96e58a"},
 				{ID: "65107A9F-7DA3-4B41-A410-6F6D9FBD68C3", Name: "geography", HRef: "http://localhost:22400/code-lists/65107A9F-7DA3-4B41-A410-6F6D9FBD68C3"},
@@ -64,7 +64,7 @@ var MIDYEARPOPEST = Response{
 	OutputInstances: []instance{
 		{
 			DatasetID: "midyearpopest",
-			Editions:  []string{"Time-series"},
+			Editions:  []string{"time-series"},
 			Title:     "Mid Year Population Estimates",
 			CodeLists: []CodeList{{ID: "time", Name: "time", HRef: "http://localhost:22400/code-lists/time"},
 				{ID: "midyearpop", Name: "geography", HRef: "http://localhost:22400/code-lists/midyearpop"},
