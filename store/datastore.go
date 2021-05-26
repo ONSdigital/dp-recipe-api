07db9d8b2a0f0932d05b861f5990c071f8209f80
Mariana Silva <Marianasilvago1234@gmail.com>
2021-05-26 23:13:21 +0530

    migrate mongo methods to new library.

--- store/datastore.go
+++ store/datastore.go
@@ -2,9 +2,9 @@ package store
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/ONSdigital/dp-recipe-api/models"
-	"github.com/globalsign/mgo/bson"
 )
 
 // DataStore provides a datastore.Storer interface used to store, retrieve, remove or update recipes
@@ -17,11 +17,11 @@ type DataStore struct {
 // Storer represents basic data access via Get, Remove and Upsert methods (to be implemented in the future).
 type Storer interface {
 	GetRecipes(ctx context.Context, offset int, limit int) (*models.RecipeResults, error)
-	GetRecipe(id string) (*models.Recipe, error)
-	AddRecipe(item models.Recipe) error
-	UpdateAllRecipe(id string, update bson.M) (err error)
-	UpdateRecipe(recipeID string, updates models.Recipe) (err error)
-	UpdateInstance(recipeID string, instanceIndex int, updates models.Instance) (err error)
-	AddCodelist(recipeID string, instanceIndex int, currentRecipe *models.Recipe) (err error)
-	UpdateCodelist(recipeID string, instanceIndex int, codelistIndex int, updates models.CodeList) (err error)
+	GetRecipe(ctx context.Context, id string) (*models.Recipe, error)
+	AddRecipe(ctx context.Context, item models.Recipe) error
+	UpdateAllRecipe(ctx context.Context, id string, update bson.M) (err error)
+	UpdateRecipe(ctx context.Context, recipeID string, updates models.Recipe) (err error)
+	UpdateInstance(ctx context.Context, recipeID string, instanceIndex int, updates models.Instance) (err error)
+	AddCodelist(ctx context.Context, recipeID string, instanceIndex int, currentRecipe *models.Recipe) (err error)
+	UpdateCodelist(ctx context.Context, recipeID string, instanceIndex int, codelistIndex int, updates models.CodeList) (err error)
 }
